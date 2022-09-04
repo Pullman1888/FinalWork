307da8d920069d47cde135e9b3cb8b37f7870f73
Artur.Kushnerenko <a@megionsoft.ru>
2022-09-05 01:40:58 +0500

    работа c циклом

--- Program.cs
+++ Program.cs
@@ -3,3 +3,14 @@ string[] Arr;
 Console.WriteLine("Введите массив");
 string s = Console.ReadLine();
 Arr = s.Split(' ');
+var res = new string[Arr.Length];
+var size = 0;
+foreach(var v in Arr);
+{
+    if (v.Length <=3)
+{
+    res[size] = v
+    size++;
+}
+
+}
