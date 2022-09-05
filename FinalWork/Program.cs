ebcbc602efc7067efdef8c9ccb2f59730be4b927
Artur.Kushnerenko <a@megionsoft.ru>
2022-09-05 17:30:02 +0500

    работа с выводом

---
+++ FinalWork/Program.cs
@@ -0,0 +1,20 @@
+﻿//FinalWork
+string[] Arr;
+Console.WriteLine("Введите массив");
+string s = Console.ReadLine();
+Arr = s.Split(' ');
+var res = new string[Arr.Length];
+var size = 0;
+foreach(var Value in Arr)
+    {
+    if (Value.Length <=3)
+{
+    res[size] = Value;
+    size++;
+}
+//Console.WriteLine(res);
+}
+    
+Console.WriteLine(string.Join(Environment.NewLine, res, 0, size));
+
+
